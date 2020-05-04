8dafb01eadc62c3f8f8d01634b4a4d77764f05f7
Loïc Hermann <loic.hermann@sciam.fr>
2020-05-04 14:17:58 +0200

    add @DefaultBean for HazelcastInstance Producer

--- rcast-hazelcast/src/main/java/fr/sciam/rcast/hazelcast/Producers.java
+++ rcast-hazelcast/src/main/java/fr/sciam/rcast/hazelcast/Producers.java
@@ -3,6 +3,7 @@ package fr.sciam.rcast.hazelcast;
 import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
+import io.quarkus.arc.DefaultBean;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -14,12 +15,14 @@ public class Producers {
     String appName;
 
     @Produces
+    @DefaultBean
     @ApplicationScoped
     HazelcastInstance createInstance(Config config){
         return Hazelcast.newHazelcastInstance(config);
     }
 
     @Produces
+    @DefaultBean
     Config createConfig(){
         var config = new Config();
         config.setInstanceName(appName);
